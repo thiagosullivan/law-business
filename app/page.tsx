840165ab1e8aa57aaa3d55f45835356f0edffbd6
thiagosullivan <thiagosullivan12@gmail.com>
2025-11-02 22:59:48 -0300

    feat: add shadcn to the project

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <div>
       <h1 className="text-red-600"> Hello World</h1>
+      <Button>Botão</Button>
     </div>
   );
 }
